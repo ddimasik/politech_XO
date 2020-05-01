cecb1fa9dcddd15da7c1831a36a3cd48b77eed36
Dmitry Glushkov <ddimasik@gmail.com>
2020-05-01 18:57:59 +0300

    mandatory part finished

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from math import sqrt
-
-
 def show_field(current_state, size=3):
     cell_num = 1
 
     for line in range(size):
         for cell in range(size):
-            print(f"[ {current_state[cell_num - 1]} {cell_num} ]", end='')
+            print(f" {current_state[cell_num - 1]} {cell_num}", end='')
             cell_num += 1
         print("\n")
     print("\n")
@@ -27,9 +24,9 @@ def make_move(current_state, gamer):
     while True:
         print(f"{'X' if gamer else 'O'}'s turn")
         print("Enter cell number", end='')
-        
+
         cell_num = process_user_input()
-        
+
         if cell_num > len(current_state) or cell_num <= 0:
             print(f"Cell number should be between 1 and {len(current_state)} but was {cell_num}")
             print("try another one, please")
@@ -45,26 +42,23 @@ def make_move(current_state, gamer):
     return current_state
 
 
-def check_final(current_state):
-    show_field(current_state)
+def check_final(f):
+    show_field(f)
 
-    def check_line(line):
-        item_compare_with = line[0]
-        for item in line:
+    def check_line(line_in_checkline):
+        item_compare_with = line_in_checkline[0]
+        for item in line_in_checkline:
             if item == '_' or item != item_compare_with: return False
         return True
 
-    size = int(sqrt(len(current_state)))
+    all_wins_lines = [[f[0], f[1], f[2]], [f[3], f[4], f[5]], [f[6], f[7], f[8]],
+                      [f[0], f[3], f[6]], [f[1], f[4], f[7]], [f[2], f[5], f[8]],
+                      [f[0], f[4], f[8]], [f[2], f[4], f[6]]
+                      ]
 
-    def chunks(lst, n):
-        """Yield successive n-sized chunks from lst."""
-        for i in range(0, len(lst), n):
-            yield lst[i:i + n]
-
-    for line in chunks(current_state, size):
+    for line in all_wins_lines:
         if check_line(line):
-            # gamer = 'winner', game finished
-            return False
+            return False  # gamer = 'winner', game finished
 
     return True
 
@@ -85,7 +79,6 @@ def make_turn():
 
 
 def end_game():
-    
     print("Press 8 to start new game, or 13 to finish", end='')
     usr_input = process_user_input()
     if usr_input == 8:
